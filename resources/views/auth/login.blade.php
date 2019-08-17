5d98c31a5e8852eb9be4a28e94ef51245b499284
juanosio <juanosio14@gmail.com>
2019-08-17 15:04:15 -0400

    Personalizando menu y title de la página

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.loginLayout')
 
+@section('title', 'Bienen System')
+
 @section('content')
 <form method="POST" action="{{ route('login') }}">
     @csrf
--- resources/views/layouts/admin/mainMenu.blade.php
+++ resources/views/layouts/admin/mainMenu.blade.php
@@ -6,7 +6,7 @@
                     <li class="active"><a data-toggle="tab" href="#Home"><i class="notika-icon notika-house"></i>
                             Inicio</a>
                     </li>
-                    <li><a data-toggle="tab" href="#mailbox"><i class="notika-icon notika-mail"></i> Email</a>
+                    <li><a data-toggle="tab" href="#planification"><i class="notika-icon notika-calendar"></i> Planificación</a>
                     </li>
                     <li><a data-toggle="tab" href="#Interface"><i class="notika-icon notika-edit"></i> Interface</a>
                     </li>
@@ -25,13 +25,15 @@
                     <div id="Home" class="tab-pane in active notika-tab-menu-bg animated flipInX">
 
                     </div>
-                    <div id="mailbox" class="tab-pane notika-tab-menu-bg animated flipInX">
+                    <div id="planification" class="tab-pane notika-tab-menu-bg animated flipInX">
                         <ul class="notika-main-menu-dropdown">
-                            <li><a href="inbox.html">Inbox</a>
+                            <li><a href="inbox.html">Crear</a>
                             </li>
-                            <li><a href="view-email.html">View Email</a>
+                            <li><a href="view-email.html">Revisar Anteriores</a>
                             </li>
-                            <li><a href="compose-email.html">Compose Email</a>
+                            <li><a href="compose-email.html">Estadísticas</a>
+                            </li>
+                            <li><a href="compose-email.html">Reportes</a>
                             </li>
                         </ul>
                     </div>
