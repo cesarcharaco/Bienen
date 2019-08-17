03a862ca8a42fc57fe3a64c4cadce10bef33776f
juanosio <juanosio14@gmail.com>
2019-08-17 14:39:32 -0400

    Personalizando el admin

---
+++ resources/views/layouts/admin/statusArea.blade.php
@@ -0,0 +1,47 @@
+<div class="notika-status-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
+                    <div class="website-traffic-ctn">
+                        <br>
+
+                        <h2>Crear Planificación</h2>
+                    </div>
+                    <div style="margin-left: 20px; padding-top: 10px">
+                        <a href="">
+                            <i class="notika-icon notika-calendar float-right" style="font-size: 30px"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
+                    <div class="website-traffic-ctn">
+                        <h2><span class="counter">90,000</span>k</h2>
+                        <p>Empleados</p>
+                    </div>
+                    <div class="sparkline-bar-stats2">1,4,8,3,5,6,4,8,3,3,9,5</div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30 dk-res-mg-t-30">
+                    <div class="website-traffic-ctn">
+                        <h2>$<span class="counter">40,000</span></h2>
+                        <p>Total Online Sales</p>
+                    </div>
+                    <div class="sparkline-bar-stats3">4,2,8,2,5,6,3,8,3,5,9,5</div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30 dk-res-mg-t-30">
+                    <div class="website-traffic-ctn">
+                        <h2><span class="counter">1,000</span></h2>
+                        <p>Total Support Tickets</p>
+                    </div>
+                    <div class="sparkline-bar-stats4">2,4,8,4,5,7,4,7,3,5,7,5</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
