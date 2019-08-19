ffa8c78f4ed7c157c00ca4454071859231520e26
juanosio <juanosio14@gmail.com>
2019-08-19 11:40:46 -0400

    Cambios en el menú y el footer

--- resources/views/layouts/appLayout.blade.php
+++ resources/views/layouts/appLayout.blade.php
@@ -122,8 +122,7 @@
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="footer-copy-right">
-                        <p>Copyright © 2018
-                            . All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
+                        <p>Derechos reservados © EICHE.</p>
                     </div>
                 </div>
             </div>
