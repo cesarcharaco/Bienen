dc075ecff6adb510e11de4b1715c08f75ab739e8
ccharaco <jcesarchg9@gmail.com>
2019-08-19 12:22:20 -0400

    Funcion para contar empleados en home

--- app/Http/helper.php
+++ app/Http/helper.php
@@ -4,3 +4,10 @@ function active($path){
    // dd(request()->is($path));
     return request()->is($path) ? "active" : " ";
 }
+
+function total_empleados()
+{
+
+	$total=App\Empleados::count();
+	return $total;
+}
