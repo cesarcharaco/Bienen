ce157b96bee5a52c31c7a2bd37a2766c75151e82
antonileon <antonijleon@gmail.com>
2019-09-27 21:03:36 -0430

    Privilegios

--- resources/views/layouts/admin/mainMenu.blade.php
+++ resources/views/layouts/admin/mainMenu.blade.php
@@ -2,13 +2,12 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <ul class="nav nav-tabs notika-menu-wrap menu-it-icon-pro">    
-                    <li class="{{ active('home') }} {{ active('estadisticas') }}"><a data-toggle="tab" href="#home"><i class="notika-icon notika-house"></i> Inicio</a>
-                    </li>
-                    <li class="{{ active('planificacion') }}"><a data-toggle="tab" href="#planification"><i class="notika-icon notika-calendar"></i> Planificación</a>
-                    </li>
-                    <li class="{{ active('empleados') }}"><a data-toggle="tab" href="#employer"><i class="notika-icon notika-support"></i> Empleados</a>
-                    </li>
+                <ul class="nav nav-tabs notika-menu-wrap menu-it-icon-pro">
+                    <li class="{{ active('home') }} {{ active('estadisticas') }}"><a data-toggle="tab" href="#home"><i class="notika-icon notika-house"></i> Inicio</a></li>
+                    @if(buscar_p('Planificación','Buscar')=="Activo" || buscar_p('Planificación','Registrar')=="Activo" || buscar_p('Planificación','Modificar')=="Activo" || buscar_p('Planificación','Eliminar')=="Activo" || buscar_p('Planificación','Aprobar')=="Activo" || buscar_p('Planificación','Reportes')=="Activo" || buscar_p('Planificación','PDF')=="Activo")
+                    <li class="{{ active('planificacion') }}"><a data-toggle="tab" href="#planification"><i class="notika-icon notika-calendar"></i> Planificación</a></li>
+                    @endif
+                    <li class="{{ active('empleados') }}"><a data-toggle="tab" href="#employer"><i class="notika-icon notika-support"></i> Empleados</a></li>
                 </ul>
 
                 <div class="tab-content custom-menu-content">
@@ -31,9 +30,9 @@
                             </li>
                             <li><a href="{{ route('planificacion.create') }}">Crear actividad</a>
                             </li>
-                            <li><a href="compose-email.html">Estadísticas</a>
+                            <li><a href="#">Estadísticas</a>
                             </li>
-                            <li><a href="compose-email.html">Reportes</a>
+                            <li><a href="#">Reportes</a>
                             </li>
                         </ul>
                     </div>
