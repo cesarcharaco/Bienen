be86da2143712f08a3d941639d31c50436036b1f
antonileon <antonijleon@gmail.com>
2019-10-02 21:00:33 -0400

    cambios en form input file

--- resources/views/planificacion/formWizard/filesForm.blade.php
+++ resources/views/planificacion/formWizard/filesForm.blade.php
@@ -1,42 +1,32 @@
-<div class="dropzone-area">
+    <!-- Dropzone area Start-->
+    <div class="dropzone-area">
         <div class="container">
             <div class="row">
                 <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-                    <div class="dropdone-nk mg-t-50">
+                    <div class="dropdone-nk mg-t-30">
                         <div class="cmp-tb-hd">
                             <h2>Cargar archivos <span style="color:red">*</span></h2>
-                            <p>Realiza la carga de tantos archivos como quieras.</p>
+                            <!-- <p>Realiza la carga de tantos archivos como quieras.</p> -->
                         </div>
-                        <div id="dropzone1" class="multi-uploader-cs">
-                            <div class="dz-message needsclick download-custom">
-                                <i class="notika-icon notika-cloud"></i>
-                                <h2>Cargar archivos.</h2>
-                                <p><span class="note needsclick">Solo se permiten formatos: .pdf, .pptx, .doc(Esto es un demo.)</span>
-                                </p>
-                            </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <input type="file" class="form-control">
                         </div>
                     </div>
                 </div>
             </div>
-
-
             <div class="row">
                 <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-                    <div class="dropdone-nk mg-t-50">
+                    <div class="dropdone-nk mg-t-30">
                         <div class="cmp-tb-hd">
                             <h2>Cargar imagenes <span style="color:red">*</span></h2>
-                            <p>Realiza la carga de varias imagenes a la vez.</p>
+                            <!-- <p>Realiza la carga de varias imagenes a la vez.</p> -->
                         </div>
-                        <div id="dropzone1" class="multi-uploader-cs">
-                            <div class="dz-message needsclick download-custom">
-                                <i class="notika-icon notika-cloud"></i>
-                                <h2>Cargar imagenes.</h2>
-                                <p><span class="note needsclick">Solo se permiten formatos: .jpg, .png(Esto es un demo.)</span>
-                                </p>
-                            </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <input type="file" class="form-control">
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <!-- Dropzone area End-->
