8432b9feb216dce4efa5a89cbba330efedd32730
ccharaco <jcesarchg9@gmail.com>
2019-10-10 10:04:17 -0400

    cambios para uso de semana actual

--- app/Http/Controllers/PlanificacionController.php
+++ app/Http/Controllers/PlanificacionController.php
@@ -53,14 +53,14 @@ class PlanificacionController extends Controller
             $gerencias1=Gerencias::where('gerencia','NPI')->first();
             $gerencias2=Gerencias::where('gerencia','CHO')->first();
             
-            /*
-            Par mostrar las planificaciones de la semana actual
+            
+            //Par mostrar las planificaciones de la semana actual
             $planificacion1 = Planificacion::where('semana',$num_semana_actual)->where('id_gerencia',1)->first();
-            $planificacion2 = Planificacion::where('semana',$num_semana_actual)->where('id_gerencia',2)->first();*/
+            $planificacion2 = Planificacion::where('semana',$num_semana_actual)->where('id_gerencia',2)->first();
             //para prueba
-            $planificacion1 = Planificacion::where('semana',38)->where('id_gerencia',1)->first();
+            /*$planificacion1 = Planificacion::where('semana',38)->where('id_gerencia',1)->first();
             $planificacion2 = Planificacion::where('semana',38)->where('id_gerencia',2)->first();
-            $num_semana_actual=38;
+            $num_semana_actual=38;*/
             //------------------------------
             $planificacion = Planificacion::where('semana','>=',$num_semana_actual)->get();
             $areas=Areas::all();
