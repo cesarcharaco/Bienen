bd2224b7b3fdfa1dd989ed5e0543d811c8c17f6f
ccharaco <jcesarchg9@gmail.com>
2019-10-18 21:57:32 -0400

    probando mostrar imagenes

--- resources/views/planificacion/create.blade.php
+++ resources/views/planificacion/create.blade.php
@@ -1102,7 +1102,8 @@ function editar_act(id_actividad) {
                     $("#mis_imagenes").empty();
                     for (var i = 0; i < data.length; i++) {
                         console.log(data[i].url);
-                        $("#mis_imagenes").append("<li><img src='{{ asset('"+data[i].url+"') }}' width='15px' height='15px' /></li>");
+                        var img="{{ asset('"+data[i].url+"') }}";
+                        $("#mis_imagenes").append("<li><img src='"+img+"' width='15px' height='15px' /></li>");
                         $("#mis_imagenes").append("<li>"+data[i].url+"</li>");
                     }
                 }
