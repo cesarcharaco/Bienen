89d37506710937c57d56354cc9847b413557b8c6
antonileon <antonijleon@gmail.com>
2019-11-27 20:48:21 -0400

    se agrego boton en el modal del home

--- resources/views/partials/modalActividades.blade.php
+++ resources/views/partials/modalActividades.blade.php
@@ -241,6 +241,7 @@
 
                 </div>
                 <div class="modal-footer mt-4">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Culminada</button>
                     <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
                 </div>
             </div>
