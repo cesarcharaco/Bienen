2f84b4875631ea4db45a34bc2e3823ae0f019889
Jhon-Dante <imagine.breacker@gmail.com>
2020-01-30 09:29:10 -0400

    Esconder footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Start Footer area-->
-<div class="footer-copyright-area" style="position: absolute !important; bottom: 0 !important; left: 0; width: 100%;">
+<!-- <div class="footer-copyright-area" style="position: absolute !important; bottom: 0 !important; left: 0; width: 100%;">
     <div class="container">
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
@@ -9,5 +9,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 <!-- End Footer area
