7317a336cc1cce3bcf340806fe68c3e2e057bfa0
Jhon-Dante <imagine.breacker@gmail.com>
2020-02-10 23:03:00 -0400

    Scrollbar registro empleados

--- resources/views/empleados/modales/registrar_empleado.blade.php
+++ resources/views/empleados/modales/registrar_empleado.blade.php
@@ -202,31 +202,33 @@
                                         <div class="form-element-list">
                                             @csrf
                                                 <h4>Cursos realizados</h4>
-                                                @foreach($cursos as $key)
-                                                    <div class="form-group">
-                                                        <div class="row">
-                                                            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                                                                <input type="checkbox" name="id_curso[]" value="Si">
-                                                            </div>
-                                                            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                                                                <label>{{$key->curso}}</label>
-                                                            </div>
-                                                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                                <div class="form-group">
-                                                                    <label>Fecha de culminación del curso</label>
-                                                                    <input type="date" name="fecha_realizado_c[]" class="form-control">
+                                                <div class="scrollbar">
+                                                    @foreach($cursos as $key)
+                                                        <div class="form-group">
+                                                            <div class="row">
+                                                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
+                                                                    <input type="checkbox" name="id_curso[]" value="Si">
                                                                 </div>
-                                                            </div>
-                                                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                                <div class="form-group">
-                                                                    <label>Fecha de vencimiento del curso</label>
-                                                                    <input type="date" name="fecha_vencimiento_c[]" class="form-control">
+                                                                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                                                    <label>{{$key->curso}}</label>
+                                                                </div>
+                                                                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                                                                    <div class="form-group">
+                                                                        <label>Fecha de culminación del curso</label>
+                                                                        <input type="date" name="fecha_realizado_c[]" class="form-control">
+                                                                    </div>
+                                                                </div>
+                                                                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                                                                    <div class="form-group">
+                                                                        <label>Fecha de vencimiento del curso</label>
+                                                                        <input type="date" name="fecha_vencimiento_c[]" class="form-control">
+                                                                    </div>
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                    </div>
-                                                    <br>
-                                                @endforeach()
+                                                        <br>
+                                                    @endforeach()
+                                                </div>
                                         </div>
 
                                     </div>
@@ -241,31 +243,33 @@
                                             @csrf
                                                 <h4>Exámenes</h4>
                                                 <br>
-                                                @foreach($examenes as $key)
-                                                    <div class="form-group">
-                                                        <div class="row">
-                                                            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                                                                <input type="checkbox" name="id_examen[]" value="Si">
-                                                            </div>
-                                                            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                                                                <label>{{$key->examen}}</label>
-                                                            </div>
-                                                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                                <div class="form-group">
-                                                                    <label>Fecha en que se realizó el examen</label>
-                                                                    <input type="date" name="fecha_realizado[]" class="form-control">
+                                                <div class="scrollbar">
+                                                    @foreach($examenes as $key)
+                                                        <div class="form-group">
+                                                            <div class="row">
+                                                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
+                                                                    <input type="checkbox" name="id_examen[]" value="Si">
                                                                 </div>
-                                                            </div>
-                                                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                                <div class="form-group">
-                                                                    <label>Fecha de vencimiento</label>
-                                                                    <input type="date" name="fecha_vencimiento[]" class="form-control">
+                                                                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                                                    <label>{{$key->examen}}</label>
+                                                                </div>
+                                                                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                                                                    <div class="form-group">
+                                                                        <label>Fecha en que se realizó el examen</label>
+                                                                        <input type="date" name="fecha_realizado[]" class="form-control">
+                                                                    </div>
+                                                                </div>
+                                                                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                                                                    <div class="form-group">
+                                                                        <label>Fecha de vencimiento</label>
+                                                                        <input type="date" name="fecha_vencimiento[]" class="form-control">
+                                                                    </div>
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                    </div>
-                                                    <br>
-                                                @endforeach()
+                                                        <br>
+                                                    @endforeach()
+                                                </div>
 
                                         </div>
 
