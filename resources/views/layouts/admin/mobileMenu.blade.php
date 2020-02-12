063c9eda8bc6ff2e38e4f7f83ca2c1889128250c
Jhon-Dante <imagine.breacker@gmail.com>
2020-02-11 20:30:21 -0400

    Arreglo permisos mobileMenu

--- resources/views/layouts/admin/mobileMenu.blade.php
+++ resources/views/layouts/admin/mobileMenu.blade.php
@@ -14,25 +14,27 @@
                                     @endif
                                 </ul>
                             </li> -->
+                            @if(buscar_p('Planificacion','Buscar')=="Si" || buscar_p('Actividades','Ver')=="Si")
                             <li><a data-toggle="collapse" data-target="#demoevent" href="#">Actividades</a>
                                 <ul id="demoevent" class="collapse dropdown-header-top">
-                                    <!-- @if(buscar_p('Planificación','Buscar')=="Si")
+                                    @if(buscar_p('Planificación','Buscar')=="Si")
                                     <li><a href="{{ route('planificacion.index') }}">Planificación</a></li>
-                                    @endif -->
+                                    @endif
+
+
                                     @if(buscar_p('Actividades','Ver')=="Si")
                                     <li><a href="{{ route('planificacion.create') }}">Actividades</a></li>
                                     @endif
+
+                                    @if(buscar_p('Actividades','Asignar')=="Si")
                                     <li><a href="{{ route('asignaciones.index') }}">Asignación</a></li>
-                                    <li><a href="{{ route('asignaciones.create') }}">Actividades asignadas</a></li>
+                                    @endif
+                                    <!-- <li><a href="{{ route('asignaciones.create') }}">Actividades asignadas</a></li> -->
                                 </ul>
                             </li>
+                            @endif
                             @if(buscar_p('Usuarios','Listado')=="Si")
-                            <li><a data-toggle="collapse" data-target="#democrou" href="#">Empleados</a>
-                                <ul id="democrou" class="collapse dropdown-header-top">
-                                    <li><a href="{{ route('empleados.index') }}">Ver</a></li>
-                                    <li><a href="{{ route('empleados.create') }}">Registrar</a></li>
-                                </ul>
-                            </li>
+                            <li><a data-toggle="collapse" data-target="#Charts" href="{{ route('empleados.index') }}">Empleados</a></li>
                             @endif
                             @if(buscar_p('Graficas','Ver')=="Si")
                             <li><a data-toggle="collapse" data-target="#Charts" href="{{ route('graficas.index') }}">Gráficas</a></li>
@@ -40,14 +42,29 @@
                             @if(buscar_p('Reportes','Excel')=="Si" || buscar_p('Reportes','PDF')=="Si")
                             <li><a data-toggle="collapse" data-target="#Charts" href="{{ route('reportes.index') }}">Reportes</a></li>
                             @endif
+
+                            @if(\Auth::user()->tipo_user == 'Admin')
                             <li>
                                 <a data-toggle="collapse" data-target="#Miscellaneousmob" href="#">Configuraciones</a>
                                 <ul id="Miscellaneousmob" class="collapse dropdown-header-top">
+                                    @if(buscar_p('Gerencias','listado')=="Si")
                                     <li><a href="{{ route('gerencias.index') }}">Gerencias</a></li>
-                                    <li><a href="{{ route('areas.index') }}">Áreas</a></li> 
+                                    @endif
+
+                                    @if(buscar_p('Areas','listado')=="Si")
+                                    <li><a href="{{ route('areas.index') }}">Áreas</a></li>
+                                    @endif
+
+                                    @if(buscar_p('Departamentos','listado')=="Si")
                                     <li><a href="{{ route('departamentos.index') }}">Departamentos</a></li> 
+                                    @endif
+
+                                    @if(\Auth::user()->tipo_user == 'Admin')
+                                    <li><a href="{{ route('privilegios.index') }}">Permisos</a></li>
+                                    @endif
                                 </ul>
                             </li>
+                            @endif
                         </ul>
                     </nav>
                 </div>
