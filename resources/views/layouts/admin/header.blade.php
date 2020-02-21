7979cfb2e782c6acf450e92c54d5f0094ca394be
ccharaco <jcesarchg9@gmail.com>
2020-02-21 01:15:53 -0400

    asignaciones de trello

--- resources/views/layouts/admin/header.blade.php
+++ resources/views/layouts/admin/header.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
                 <div class="logo-area1">
-                    <a href="#"><img src="{{ asset('assets/img/logo_lica_1.png') }}" alt="Logo" height="40px" width="100px;" class="mb-1 mt-1" /></a>
+                    <a href="{{ url('home') }}"><img src="{{ asset('assets/img/logo_lica_1.png') }}" alt="Logo" height="40px" width="100px;" class="mb-1 mt-1" title="Ir al Inicio" /></a>
                 </div>
             </div>
             <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
