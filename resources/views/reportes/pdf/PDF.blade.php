ede02aa285fd0f19bc3a94fd30e2edcb3e612cce
ccharaco <jcesarchg9@gmail.com>
2020-05-02 11:04:50 -0400

    cambiando estilos en el pdf

--- resources/views/reportes/pdf/PDF.blade.php
+++ resources/views/reportes/pdf/PDF.blade.php
@@ -33,37 +33,37 @@
 </head>
 <body>
 <?php $image_path = '/assets/img/bienen.jpg'; ?>
-<table border="1px" width="100%">
+<table border="1px" width="100vw">
   <thead>
     @for($i=0; $i<count($planificacion);$i++)
     <tr>
       <td style="font-size: 10px; height: 30px;" rowspan="3" id="cell">
         <img src="{{ asset('assets/images/checked.png') }}" style="border-radius: 30px !important;" height="15px" width="15px"/>Asignada</td>
-      <td colspan="11" style="font-size: 10px; text-align: center; background: #D6EAF8;">REPORTE ACTIVIDAD SEMANAL</td>
+      <td colspan="10" style="font-size: 10px; text-align: center; background: #D6EAF8;">REPORTE ACTIVIDAD SEMANAL</td>
     </tr>
     <tr style="font-size: 10px;">
-      <td colspan="3">@if($cant_act[$i]>0)Área: {{ $areas[$i] }} @endif</td>
+      <td colspan="2">@if($cant_act[$i]>0)Área: {{ $areas[$i] }} @endif</td>
       <td colspan="3">Elaborado:{{ $planificacion[$i][0] }}</td>
       <td colspan="5">N° de contrato:{{ $planificacion[$i][2] }}</td>      
     </tr>
     <tr style="font-size: 10px;">
-      <td colspan="3">Fecha:{{ $planificacion[$i][3] }}</td>
+      <td colspan="2">Fecha:{{ $planificacion[$i][3] }}</td>
       <td colspan="3">Aprobado por:{{ $planificacion[$i][1] }}</td>
       <td colspan="5">Revisión: {{ $planificacion[$i][5] }}</td>
     </tr>
     <tr style="font-size: 10px;" align="center">
-        <th style="background: #F9E79F;" height="30">Task</th>
-        <th style="background: #F9E79F;">Descripción</th>
-        <th style="background: #F9E79F;">Date</th>
-        <th style="background: #F9E79F;">Duración proyectada</th>
-        <th style="background: #F9E79F;">Cant. personas</th>
-        <th style="background: #F9E79F;">Duración real</th>
-        <th style="background: #F9E79F;">Día {{ $cant_mie }}</th>
-        <th style="background: #F9E79F;">Tipo</th>
-        <th style="background: #F9E79F;">Departamento</th>
-        <th style="background: #F9E79F;">Realizada SI/NO</th>
-        <th style="background: #F9E79F;">Observaciones</th>
-        <th style="background: #F9E79F;" class="maximos" width="20vw">Comentarios</th>
+        <th style="background: #F9E79F; width: 10%" height="30">Task</th>
+        {{-- <th style="background: #F9E79F; width: 10%">Descripción</th> --}}
+        <th style="background: #F9E79F; width: 3%">Date</th>
+        <th style="background: #F9E79F; width: 1%">Duración proyectada</th>
+        <th style="background: #F9E79F; width: 1%">Cant. personas</th>
+        <th style="background: #F9E79F; width: 1%">Duración real</th>
+        <th style="background: #F9E79F; width: 1%">Día {{ $cant_mie }}</th>
+        <th style="background: #F9E79F; width: 1%">Tipo</th>
+        <th style="background: #F9E79F; width: 3%">Departamento</th>
+        <th style="background: #F9E79F; width: 2%">Realizada SI/NO</th>
+        <th style="background: #F9E79F; width: 5%">Observaciones</th>
+        <th style="background: #F9E79F; width: 10%">Comentarios</th>
     </tr>
   </thead>
   <tbody>
@@ -86,8 +86,8 @@
             <img src="{{ asset('assets/images/checked.png') }}" style="border-radius: 30px !important;" height="15px" width="15px"/>
           @endif 
           {{ $actividades[$i][$j][0] }}</td>
-          <td>{{ $actividades[$i][$j][1] }}</td>
-          <td>{{ $actividades[$i][$j][3] }}</td>
+          {{-- <td>{{ $actividades[$i][$j][1] }}</td> --}}
+          <td>{{ date("d-m-Y", strtotime($actividades[$i][$j][3])) }}</td>
           <td>{{ $actividades[$i][$j][4] }}</td>
           <td>{{ $actividades[$i][$j][5] }}</td>
           <td>{{ $actividades[$i][$j][6] }}</td>
@@ -100,7 +100,7 @@
           </td>
           <td>{{ $actividades[$i][$j][9] }}</td>
           <td>{{ $actividades[$i][$j][11] }}</td>
-          <td class="maximos" width="20vw" >
+          <td>
             @if(actividad_asignada($actividades[$i][$j][13])>0) 
                 {{ comentarios_actividad($actividades[$i][$j][13]) }}
             @endif
@@ -110,7 +110,7 @@
       @if($x==$cant_mie)
       
       <tr style="background: black; color: white;">
-        <td colspan="2">Total</td>
+        <td >Total</td>
         <td>{{ $tdp_mie }}</td>
         <td></td>
         <td>{{ $tdr_mie }}</td>
@@ -118,17 +118,17 @@
       </tr>
       <tr>
         <td colspan="3" style="background: yellow;">{{ $actividades[$i][$j][12] }}</td>
-        <td colspan="9" style="background: yellow; text-align: center;">{{ $actividades[$i][$j][12] }}</td>
+        <td colspan="8" style="background: yellow; text-align: center;">{{ $actividades[$i][$j][12] }}</td>
       </tr>
       <tr>
-        <td colspan="12"></td>
+        <td colspan="11"></td>
       </tr>
       @endif
       {{-- totales dia jueves --}}
       @if($y==$x)
       
       <tr style="background: black; color: white;">
-        <td colspan="2">Total</td>
+        <td >Total</td>
         <td>{{ $tdp_jue }}</td>
         <td></td>
         <td>{{ $tdr_jue }}</td>
@@ -136,17 +136,17 @@
       </tr>
       <tr>
         <td colspan="3" style="background: yellow;">{{ $actividades[$i][$j][12] }}</td>
-        <td colspan="9" style="background: yellow; text-align: center;">{{ $actividades[$i][$j][12] }}</td>
+        <td colspan="8" style="background: yellow; text-align: center;">{{ $actividades[$i][$j][12] }}</td>
       </tr>
       <tr>
-        <td colspan="12"></td>
+        <td colspan="11"></td>
       </tr>
       @endif
       {{-- totales dia viernes --}}
       @if($z==$x)
       
       <tr style="background: black; color: white;">
-        <td colspan="2">Total</td>
+        <td >Total</td>
         <td>{{ $tdp_vie }}</td>
         <td></td>
         <td>{{ $tdr_vie }}</td>
@@ -154,17 +154,17 @@
       </tr>
       <tr>
         <td colspan="3" style="background: yellow;">{{ $actividades[$i][$j][12] }}</td>
-        <td colspan="9" style="background: yellow; text-align: center;">{{ $actividades[$i][$j][12] }}</td>
+        <td colspan="8" style="background: yellow; text-align: center;">{{ $actividades[$i][$j][12] }}</td>
       </tr>
       <tr>
-        <td colspan="12"></td>
+        <td colspan="11"></td>
       </tr>
       @endif
       {{-- totales dia jueves --}}
       @if($aa==$x)
       
       <tr style="background: black; color: white;">
-        <td colspan="2">Total</td>
+        <td >Total</td>
         <td>{{ $tdp_sab }}</td>
         <td></td>
         <td>{{ $tdr_sab }}</td>
@@ -172,17 +172,17 @@
       </tr>
       <tr>
         <td colspan="3" style="background: yellow;">{{ $actividades[$i][$j][12] }}</td>
-        <td colspan="9" style="background: yellow; text-align: center;">{{ $actividades[$i][$j][12] }}</td>
+        <td colspan="8" style="background: yellow; text-align: center;">{{ $actividades[$i][$j][12] }}</td>
       </tr>
       <tr>
-        <td colspan="12"></td>
+        <td colspan="11"></td>
       </tr>
       @endif
       {{-- totales dia domingo --}}
       @if($bb==$x)
       
       <tr style="background: black; color: white;">
-        <td colspan="2">Total</td>
+        <td >Total</td>
         <td>{{ $tdp_dom }}</td>
         <td></td>
         <td>{{ $tdr_dom }}</td>
@@ -190,17 +190,17 @@
       </tr>
       <tr>
         <td colspan="3" style="background: yellow;">{{ $actividades[$i][$j][12] }}</td>
-        <td colspan="9" style="background: yellow; text-align: center;">{{ $actividades[$i][$j][12] }}</td>
+        <td colspan="8" style="background: yellow; text-align: center;">{{ $actividades[$i][$j][12] }}</td>
       </tr>
       <tr>
-        <td colspan="12"></td>
+        <td colspan="11"></td>
       </tr>
       @endif
       {{-- totales dia lunes --}}
       @if($cc==$x)
       
       <tr style="background: black; color: white;">
-        <td colspan="2">Total</td>
+        <td >Total</td>
         <td>{{ $tdp_lun }}</td>
         <td></td>
         <td>{{ $tdr_lun }}</td>
@@ -208,17 +208,17 @@
       </tr>
       <tr>
         <td colspan="3" style="background: yellow;">{{ $actividades[$i][$j][12] }}</td>
-        <td colspan="9" style="background: yellow; text-align: center;">{{ $actividades[$i][$j][12] }}</td>
+        <td colspan="8" style="background: yellow; text-align: center;">{{ $actividades[$i][$j][12] }}</td>
       </tr>
       <tr>
-        <td colspan="12"></td>
+        <td colspan="11"></td>
       </tr>
       @endif
       {{-- totales dia martes --}}
       @if($dd==$x)
       
       <tr style="background: black; color: white;">
-        <td colspan="2">Total</td>
+        <td >Total</td>
         <td>{{ $tdp_mar }}</td>
         <td></td>
         <td>{{ $tdr_mar }}</td>
@@ -226,10 +226,10 @@
       </tr>
       <tr>
         <td colspan="3" style="background: yellow;">{{ $actividades[$i][$j][12] }}</td>
-        <td colspan="9" style="background: yellow; text-align: center;">{{ $actividades[$i][$j][12] }}</td>
+        <td colspan="8" style="background: yellow; text-align: center;">{{ $actividades[$i][$j][12] }}</td>
       </tr>
       <tr>
-        <td colspan="12"></td>
+        <td colspan="11"></td>
       </tr>
       @endif
     @endfor
