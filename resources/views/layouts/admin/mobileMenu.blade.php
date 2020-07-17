377840a487f9686b39d809162465d1524b82bdbf
Jhon-Dante <imagine.breacker@gmail.com>
2020-07-17 10:23:15 -0400

    Accesos ViewMel vista mobile

--- resources/views/layouts/admin/mobileMenu.blade.php
+++ resources/views/layouts/admin/mobileMenu.blade.php
@@ -5,7 +5,7 @@
                 <div class="mobile-menu">
                     <nav id="dropdown">
                         <ul class="mobile-menu-nav">
-                            <li><a data-toggle="collapse" data-target="#Charts" href="{{ route('home') }}">Dashboard</a></li>
+                            <li class="{{ active('home') }}"><a href="{{ route('home') }}" ><i class="notika-icon notika-house"></i> Dashboard </a></li>
                             <!-- <li><a data-toggle="collapse" data-target="#demolibra" href="#">Inicio</a>
                                 <ul id="demolibra" class="collapse dropdown-header-top">
                                     <li><a href="{{ route('home') }}">Dashboard</a></li>
@@ -14,7 +14,7 @@
                                     @endif
                                 </ul>
                             </li> -->
-                            @if(buscar_p('Planificacion','Buscar')=="Si" || buscar_p('Actividades','Ver')=="Si")
+                            @if((buscar_p('Planificacion','Buscar')=="Si" || buscar_p('Actividades','Ver')=="Si")  && \Auth::User()->email!="ViewMel@licancabur.cl")
                             <li><a data-toggle="collapse" data-target="#demoevent" href="#">Actividades</a>
                                 <ul id="demoevent" class="collapse dropdown-header-top">
                                     @if(buscar_p('Planificación','Buscar')=="Si")
@@ -28,17 +28,17 @@
                                 </ul>
                             </li>
                             @endif
-                            @if(buscar_p('Usuarios','Listado')=="Si")
+                           @if(buscar_p('Usuarios','Listado')=="Si" && \Auth::User()->email!="ViewMel@licancabur.cl")
                             <li><a data-toggle="collapse" data-target="#Charts" href="{{ route('empleados.index') }}">Empleados</a></li>
                             @endif
-                            @if(buscar_p('Graficas','Ver')=="Si")
+                            @if(buscar_p('Graficas','Ver')=="Si" && \Auth::User()->email!="ViewMel@licancabur.cl")
                             <li><a data-toggle="collapse" data-target="#Charts" href="{{ route('graficas.index') }}">Gráficas</a></li>
                             @endif
                             @if(buscar_p('Reportes','Excel')=="Si" || buscar_p('Reportes','PDF')=="Si")
-                            <li><a data-toggle="collapse" data-target="#Charts" href="{{ route('reportes.index') }}">Reportes</a></li>
+                            <li class="{{ active('reportes') }}"><a href="{{ route('reportes.index') }}" ><i class="fa fa-file-archive-o"></i> Reportes </a></li>
                             @endif
 
-                            @if(\Auth::user()->tipo_user == 'Admin')
+                            @if((buscar_p('Areas','Listado')=="Si" || buscar_p('Gerencias','Listado')=="Si" || buscar_p('Departamentos','Listado')=="Si") && \Auth::User()->email!="ViewMel@licancabur.cl")
                             <li>
                                 <a data-toggle="collapse" data-target="#Miscellaneousmob" href="#">Configuraciones</a>
                                 <ul id="Miscellaneousmob" class="collapse dropdown-header-top">
@@ -68,6 +68,10 @@
                                     @endif
                                 </ul>
                             </li>
+                            @endif
+                            @if(\Auth::user()->tipo_user == 'Admin')
+                                <li class="{{ active('estadisticas1*') }}"><a href="{{ route('estadisticas1.index') }}" ><i class="fa fa-th-list"></i> Estadísticas </a>
+                                </li>
                             @endif
                         </ul>
                     </nav>
