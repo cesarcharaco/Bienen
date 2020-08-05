9da0327485f55c7429546d90208fb56c8145be0f
ccharaco <jcesarchg9@gmail.com>
2020-08-05 11:30:08 -0400

    arreglando helper

--- app/Http/helper.php
+++ app/Http/helper.php
@@ -122,12 +122,14 @@ function buscar_p($modulo,$privilegio)
 {
 	$hallado="No";
 	$privilegio=App\Privilegios::where('privilegio',$privilegio)->where('modulo',$modulo)->first();
-	foreach ($privilegio->usuarios as $key) {
-		
-		if ($key->pivot->id_usuario==\Auth::user()->id) {
-			$hallado=$key->pivot->status;
-		}
-	}
+    if(!is_null($privilegio)){
+    	foreach ($privilegio->usuarios as $key) {
+    		
+    		if ($key->pivot->id_usuario==\Auth::user()->id) {
+    			$hallado=$key->pivot->status;
+    		}
+    	}
+    }
 	return $hallado;
 }
 
