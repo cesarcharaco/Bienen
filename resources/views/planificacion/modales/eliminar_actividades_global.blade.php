9e1934c5f3d98e28264d1d0fdbbf4818899d2f69
Jhon-Dante <imagine.breacker@gmail.com>
2020-10-26 13:15:03 -0400

    Ocultar contraseña Admin - Eliminar actividades

--- resources/views/planificacion/modales/eliminar_actividades_global.blade.php
+++ resources/views/planificacion/modales/eliminar_actividades_global.blade.php
@@ -7,11 +7,13 @@
             <div class="modal-body">
                 <h2>ATENCIÓN! Está a punto de eliminar todas las ctividades en la planificación, área y tipo de actividad seleccionados!</h2>
 
-                <div class="form-group">
-                    <label class="text-danger">Ingrese su contraseña de usuario *</label>
-                    <input type="password" name="contraseña_conf" class="form-control" id="contraseña_confir2" onkeyup="VerificaContraseña(1,this.value)" required value="" placeholder="Contraseña">
-                    <span class="contraseña_incorrecta"></span>
-                </div>
+                @if(\Auth::user()->tipo_user!="Admin")
+                    <div class="form-group">
+                        <label class="text-danger">Ingrese su contraseña de usuario *</label>
+                        <input type="password" name="contraseña_conf" class="form-control" id="contraseña_confir2" onkeyup="VerificaContraseña(1,this.value)" required value="" placeholder="Contraseña">
+                        <span class="contraseña_incorrecta"></span>
+                    </div>
+                @endif
                 <p>Esta acción no se podra deshacer en el futuro.</p>
             
                 <div class="modal-footer">
@@ -19,7 +21,11 @@
                     <input type="hidden" name="id_actividad_eliminar" id="id_actividad_eliminar">
                     <input type="hidden" name="id_empleado_act_eliminar" id="id_empleado_act_eliminar">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-                    <button type="submit" class="btn btn-default" id="botonEliminarG" disabled data-toggle="modal">Eliminar</button>
+                    @if(\Auth::user()->tipo_user!="Admin")
+                        <button type="submit" class="btn btn-default" id="botonEliminarG" disabled data-toggle="modal">Eliminar</button>
+                    @else
+                        <button type="submit" class="btn btn-default" id="botonEliminarG" data-toggle="modal">Eliminar</button>
+                    @endif
                 </div>
             </div>
         </div>
--- resources/views/planificacion/modales/eliminar_actividades_global2.blade.php
+++ resources/views/planificacion/modales/eliminar_actividades_global2.blade.php
@@ -8,18 +8,24 @@
                 <h2>ATENCIÓN! Está a punto de eliminar las actividades seleccionadas!</h2>
                 <p>Esta acción no se podra deshacer en el futuro.</p>
                 
-                <div class="form-group">
-                    <label class="text-danger">Ingrese su contraseña de usuario *</label>
-                    <input type="password" name="contraseña_conf" class="form-control" id="contraseña_confir2-2" onkeyup="VerificaContraseña(2,this.value)" required value="" placeholder="Contraseña">
-                    <span class="contraseña_incorrecta"></span>
-                </div>
+                @if(\Auth::user()->tipo_user!="Admin")
+                    <div class="form-group">
+                        <label class="text-danger">Ingrese su contraseña de usuario *</label>
+                        <input type="password" name="contraseña_conf" class="form-control" id="contraseña_confir2-2" onkeyup="VerificaContraseña(2,this.value)" required value="" placeholder="Contraseña">
+                        <span class="contraseña_incorrecta"></span>
+                    </div>
+                @endif
 
                 <div class="modal-footer">
                     <!-- <button type="button" class="btn btn-default" data-toggle="modal" data-target="#claveroot">Eliminar</button> -->
                     <input type="hidden" name="id_actividad_eliminar" id="id_actividad_eliminar">
                     <input type="hidden" name="id_empleado_act_eliminar" id="id_empleado_act_eliminar">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-                    <button type="submit" class="btn btn-default" id="botonEliminarE" disabled data-toggle="modal">Eliminar</button>
+                    @if(\Auth::user()->tipo_user!="Admin")
+                        <button type="submit" class="btn btn-default" id="botonEliminarE" disabled data-toggle="modal">Eliminar</button>
+                    @else
+                        <button type="submit" class="btn btn-default" id="botonEliminarE" disabled data-toggle="modal">Eliminar</button>
+                    @endif
                 </div>
             </div>
         </div>
