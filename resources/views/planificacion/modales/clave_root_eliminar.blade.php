d1a5f53f5ff6e98ee73e4edeab71a2e3f949b485
Jhon-Dante <imagine.breacker@gmail.com>
2020-10-26 13:33:16 -0400

    Ocultar contraseña Admin - Eliminar actividad

--- resources/views/planificacion/modales/clave_root_eliminar.blade.php
+++ resources/views/planificacion/modales/clave_root_eliminar.blade.php
@@ -6,26 +6,21 @@
             </div>
             <div class="modal-body">
                 {!! Form::open(['route' => ['actividades.destroy',1033], 'method' => 'DELETE']) !!}
-                            <input type="hidden" name="id_producto" id="id_producto" >
-                            <div class="row form-group">
-                                <div class="col col-md-1">
-                                    
-                                </div>
-                                
-                                <div class="col-12 col-md-9">
-                                    <label for="text-input" class=" form-control-label"> <b style="color:red;">*</b> Contraseña de Administrador</label>
-                                   <input type="password" id="clave" name="clave" class="form-control" required="required">
-                                    <small class="form-text text-muted">Escribe la contraseña e administrador para validar la eliminación</small>
-                                </div>
-                            </div>
+                    <input type="hidden" name="id_producto" id="id_producto" >
+                    <div class="row form-group">
+                        <div class="col-12 col-md-9">
+                            <label for="text-input" class=" form-control-label"> <b style="color:red;">*</b> Contraseña de Administrador</label>
+                           <input type="password" id="clave" name="clave" class="form-control" required="required">
+                            <small class="form-text text-muted">Escribe la contraseña e administrador para validar la eliminación</small>
                         </div>
+                    </div>
 
-                        <div class="modal-footer">
-                            <input type="hidden" name="id_actividad_eliminar" id="id_actividad_eliminar">
-                            <button type="button" class="btn btn-primary" data-dismiss="modal">Cancelar</button>
-                            <button type="submit" class="btn btn-primary">Confirmar</button>
-                        </div>
-                        {!! Form::close() !!}
+                    <div class="modal-footer">
+                        <input type="hidden" name="id_actividad_eliminar" id="id_actividad_eliminar">
+                        <button type="button" class="btn btn-primary" data-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-primary">Confirmar</button>
+                    </div>
+                {!! Form::close() !!}
             </div>
         </div>
     </div>
